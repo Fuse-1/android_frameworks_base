6fe63051747453d08a341e93b6742994893fec98
SakthivelNadar <s2234nadar@gmail.com>
2022-07-12 09:14:19 +0000

    Revert "Drop Type clock face."
    This reverts commit 583067c4e4799b0a633d8e861698c8fc2a5c7a6f.
    
    Signed-off-by: sushmit1 <sushmit718@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -148,6 +148,7 @@ public final class ClockManager {
         Resources res = context.getResources();
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new TypeClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
